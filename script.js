74cb92ea7fb5f247d98257a92c7cfd482d6ed799
jevinus <jevinus@protonmail.com>
2024-03-05 09:55:30 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1. # character loop 
+/* function tagLoop(char) {
+  for (let i = 0; i <= 7; i++){
+    console.log(char += char);
+  }
+}
+
+tagLoop("#"); END */
+
+// 2. fizzBuzz project 
+/* function fizzBuzz(num) {
+  for (let i = 0; i <= num; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log("fizzBuzz")
+    }  else if (i % 3 === 0) {
+      console.log("fizz")
+    } else if (i % 5 === 0) {
+      console.log("buzz")
+    } else {
+      console.log(i)
+    }
+  }
+ }
+
+ fizzBuzz(100); END */
